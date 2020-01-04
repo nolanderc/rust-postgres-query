c8065e595692fbd0cced5f7e179e2b89fdba6b5e
Christofer Nolander <christofer.nolander@gmail.com>
2020-01-04 14:54:51 +0100

    Better attribute extraction

--- postgres_query_macro/src/from_sql_row.rs
+++ postgres_query_macro/src/from_sql_row.rs
@@ -70,6 +70,28 @@ pub fn derive(input: DeriveInput) -> TokenStream {
     }
 }
 
+fn make_constructor(input: &DeriveInput, locals: impl IntoIterator<Item = Ident>) -> TokenStream {
+    let ident = &input.ident;
+
+    let mut fields = TokenStream::new();
+    fields.append_separated(locals, quote! { , });
+
+    match &input.data {
+        Data::Struct(data) => match data.fields {
+            Fields::Unnamed(_) => quote! { #ident ( #fields ) },
+            Fields::Named(_) => quote! { #ident { #fields } },
+            Fields::Unit => {
+                if fields.is_empty() {
+                    quote! { #ident }
+                } else {
+                    panic!("Attempted to construct unit struct with fields");
+                }
+            }
+        },
+        _ => panic!(),
+    }
+}
+
 struct Column {
     index: Index,
     field: Field,
@@ -81,57 +103,24 @@ enum Index {
     Name(Ident),
 }
 
-fn attribute_items<'a>(
-    name: &str,
-    attrs: impl IntoIterator<Item = &'a Attribute>,
-) -> Result<Vec<Meta>> {
-    let mut items = Vec::new();
-
-    for attr in attrs {
-        if !attr.path.is_ident(name) {
-            continue;
-        }
-
-        let meta = attr.parse_meta()?;
-        let list = match meta {
-            Meta::List(list) => list,
-            _ => return Err(Error::new(attr.span(), "expected list: #[row(...)]")),
-        };
-
-        for inner in list.nested {
-            match inner {
-                NestedMeta::Lit(_) => return Err(Error::new(inner.span(), "unexpected literal")),
-                NestedMeta::Meta(item) => items.push(item),
-            }
-        }
-    }
-
-    Ok(items)
-}
-
 fn extract_columns(input: &DeriveInput) -> Result<Vec<Column>> {
     match &input.data {
         Data::Struct(data) => {
-            let items = attribute_items("row", &input.attrs)?;
-
-            let is_ordered = has_ident("order", &items);
+            let container = ContainerAttributes::from_attrs(&input.attrs)?;
 
             let columns = data
                 .fields
                 .iter()
                 .map(|field| -> Result<_> {
-                    let items = attribute_items("row", &field.attrs)?;
-
-                    let flatten = has_ident("flatten", &items);
+                    let attrs = FieldAttributes::from_attrs(&field.attrs)?;
 
                     let index = match &field.ident {
                         None => Index::Position,
-                        Some(_) if is_ordered => Index::Position,
-                        Some(_) if flatten => Index::Flatten,
+                        Some(_) if container.order => Index::Position,
+                        Some(_) if attrs.flatten => Index::Flatten,
                         Some(name) => {
-                            if let Some(literal) = get_key("rename", &items) {
-                                let name = lit_string(literal)?;
-                                Index::Name(Ident::new(&name, literal.span()))
+                            if let Some(name) = attrs.rename {
+                                Index::Name(name)
                             } else {
                                 Index::Name(name.clone())
                             }
@@ -163,6 +152,118 @@ fn extract_columns(input: &DeriveInput) -> Result<Vec<Column>> {
     }
 }
 
+struct ContainerAttributes {
+    order: bool,
+}
+
+struct FieldAttributes {
+    flatten: bool,
+    rename: Option<Ident>,
+    split: Option<String>,
+}
+
+macro_rules! set_or_err {
+    ($item:ident, $var:ident, $val:expr) => {
+        match $var.is_some() {
+            true => Err(Error::new($item.span(), "attribute specified twice")),
+            false => {
+                $var = Some($val);
+                Ok(())
+            }
+        }
+    };
+}
+
+impl ContainerAttributes {
+    pub fn from_attrs<'a>(
+        attrs: impl IntoIterator<Item = &'a Attribute>,
+    ) -> Result<ContainerAttributes> {
+        let items = attribute_items("row", attrs)?;
+
+        let mut order = None;
+
+        for item in items {
+            match item {
+                item if meta_is_ident("order", &item) => set_or_err!(item, order, true)?,
+                item => return Err(Error::new(item.span(), "unknown attribute")),
+            }
+        }
+
+        let container = ContainerAttributes {
+            order: order.unwrap_or(false),
+        };
+
+        Ok(container)
+    }
+}
+
+impl FieldAttributes {
+    pub fn from_attrs<'a>(
+        attrs: impl IntoIterator<Item = &'a Attribute>,
+    ) -> Result<FieldAttributes> {
+        let items = attribute_items("row", attrs)?;
+
+        let mut flatten = None;
+        let mut rename = None;
+        let mut split = None;
+
+        for item in items {
+            match item {
+                item if meta_is_ident("flatten", &item) => set_or_err!(item, flatten, true)?,
+
+                Meta::NameValue(pair) if pair.path.is_ident("rename") => {
+                    let text = lit_string(&pair.lit)?;
+                    let name = Ident::new(&text, pair.lit.span());
+                    set_or_err!(pair, rename, name)?;
+                }
+
+                Meta::NameValue(pair) if pair.path.is_ident("split") => {
+                    let text = lit_string(&pair.lit)?;
+                    set_or_err!(pair, split, text)?;
+                }
+
+                item => return Err(Error::new(item.span(), "unknown attribute")),
+            }
+        }
+
+        let field = FieldAttributes {
+            flatten: flatten.unwrap_or(false),
+            rename,
+            split,
+        };
+
+        Ok(field)
+    }
+}
+
+fn attribute_items<'a>(
+    name: &str,
+    attrs: impl IntoIterator<Item = &'a Attribute>,
+) -> Result<Vec<Meta>> {
+    let mut items = Vec::new();
+
+    for attr in attrs {
+        if !attr.path.is_ident(name) {
+            continue;
+        }
+
+        let meta = attr.parse_meta()?;
+        let list = match meta {
+            Meta::List(list) => list,
+            _ => return Err(Error::new(attr.span(), "expected list: #[row(...)]")),
+        };
+
+        for inner in list.nested {
+            match inner {
+                NestedMeta::Lit(_) => return Err(Error::new(inner.span(), "unexpected literal")),
+                NestedMeta::Meta(item) => items.push(item),
+            }
+        }
+    }
+
+    Ok(items)
+}
+
 fn meta_is_ident(ident: &str, meta: &Meta) -> bool {
     match meta {
         Meta::Path(path) if path.is_ident(ident) => true,
@@ -170,45 +271,9 @@ fn meta_is_ident(ident: &str, meta: &Meta) -> bool {
     }
 }
 
-fn has_ident<'a>(ident: &str, items: impl IntoIterator<Item = &'a Meta>) -> bool {
-    items
-        .into_iter()
-        .find(|item| meta_is_ident(ident, item))
-        .is_some()
-}
-
-fn get_key<'a>(ident: &str, items: impl IntoIterator<Item = &'a Meta>) -> Option<&'a Lit> {
-    items.into_iter().find_map(|item| match item {
-        Meta::NameValue(pair) if pair.path.is_ident(ident) => Some(&pair.lit),
-        _ => None,
-    })
-}
-
 fn lit_string(lit: &Lit) -> Result<String> {
     match lit {
         Lit::Str(text) => Ok(text.value()),
         _ => Err(Error::new(lit.span(), "expected string literal")),
-    }
-}
-
-fn make_constructor(input: &DeriveInput, locals: impl IntoIterator<Item = Ident>) -> TokenStream {
-    let ident = &input.ident;
-
-    let mut fields = TokenStream::new();
-    fields.append_separated(locals, quote! { , });
-
-    match &input.data {
-        Data::Struct(data) => match data.fields {
-            Fields::Unnamed(_) => quote! { #ident ( #fields ) },
-            Fields::Named(_) => quote! { #ident { #fields } },
-            Fields::Unit => {
-                if fields.is_empty() {
-                    quote! { #ident }
-                } else {
-                    panic!("Attempted to construct unit struct with fields");
-                }
-            }
-        },
-        _ => panic!(),
     }
 }
